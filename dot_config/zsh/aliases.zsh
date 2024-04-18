17b675064d6a7b655345881f70b17aacb58fcfa1
thomas <thomas@tdlx.nl>
2024-04-18 20:55:35 +0700

    conf: updated zsh config

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,19 @@
+## ls shortcuts
+alias ll='ls -lh'
+alias lla='ls -lha'
+
+# folder shortcuts
+alias oasis="$HOME/github/oasis"
+alias home='~'  
+alias config="$HOME/github/config"
+alias code='code-insiders'
+
+## git shortcuts
+alias gl="git pull"
+alias gp="git push"
+
+alias gitstatus="cd $HOME/github/ && $HOME/github/config-station/scripts/git/mgitstatus.sh && cd -"
+
+# set neovim as default editor
+alias vim="nvim"
+alias vi="nvim"
---
+++ dot_config/zsh/variables.zsh
@@ -0,0 +1,10 @@
+# fzf configuration
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+## nvim configurations
+export VISUAL=nvim;
+export EDITOR=nvim;
+# zsh configuration
+export HISTFILE="$XDG_CONFIG_HOME/zsh/.zsh_history"
+export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
+export ZSH="$XDG_CONFIG_HOME/zsh/oh-my-zsh"
+export ZSHCUSTOM="$XDG_CONFIG_HOME/zsh"
