ec06bc5a4305bd84315139e9b9e17184212741e7
thomas <thomas@tdlx.nl>
2024-04-28 21:14:21 +0200

    conf: updated nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -26,7 +26,6 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = "unnamedplus"
-
 -- Enable break indent
 vim.opt.breakindent = true
 
--- dot_config/nvim/lua/plugins/neo-tree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    view = {
-      relativenumber = true,
-    },
-  },
-}
