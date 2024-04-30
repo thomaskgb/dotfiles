edf81e1a0d08d57d5a7ad8656eb8c5b57419ef60
thomas <thomas@tdlx.nl>
2024-04-30 13:48:37 +0200

    conf: added some aliasses to zsh
    mostly related to new chezmoi configuration

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -11,9 +11,23 @@ alias code='code-insiders'
 ## git shortcuts
 alias gl="git pull"
 alias gp="git push"
+alias gs="cd $HOME/github/ && $HOME/github/config-station/scripts/git/mgitstatus.sh && cd -"
 
-alias gitstatus="cd $HOME/github/ && $HOME/github/config-station/scripts/git/mgitstatus.sh && cd -"
+## chezmo shortcuts
+alias c="chezmoi"
+alias ce="chezmoi edit"
+alias cc="chezmoi cd"
+alias cs="chezmoi status"
+alias ca="chezmoi add"
+alias cra="chezmoi re-add" # re-add all previously added files even when the target has changes
+
+## lazy git shortcut
+alias lg="lazygit"
 
 # set neovim as default editor
 alias vim="nvim"
 alias vi="nvim"
+
+
+# tmux aliasses
+alias tns="tmux new-session -s test"
