b0f4f8729f19ccad837e8fce5a0ef6094d7c9fe2
thomas <thomas@tdlx.nl>
2024-05-15 08:28:23 +0200

    conf: added fzf ignore file & moved to fd instead of find

--- dot_config/zsh/variables.zsh
+++ dot_config/zsh/variables.zsh
@@ -1,5 +1,8 @@
 # fzf configuration
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --ignore-file ~/.config/fzf/ignore'
+export FZF_CTRL_T_COMMAND='fd --type f --hidden --ignore-file ~/.config/fzf/ignore'
+
 ## nvim configurations
 export VISUAL=nvim;
 export EDITOR=nvim;
