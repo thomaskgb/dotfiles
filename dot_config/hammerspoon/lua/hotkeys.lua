61e555268d326030ca46f98a0cd4c44010f964ed
thomas <thomas@tdlx.nl>
2026-01-30 22:59:20 +0100

    conf: Update configs, remove yabai, add chezmoiignore
    - Update zsh, tmux, karabiner, and hammerspoon configs
    - Remove yabai directory (no longer used)
    - Add .chezmoiignore to exclude nvim lazy-lock.json

--- dot_config/hammerspoon/lua/hotkeys.lua
+++ dot_config/hammerspoon/lua/hotkeys.lua
@@ -1,38 +1,81 @@
 local screenResolution = require("lua.screen_resolution")
+local appLauncher = require("lua.app_launcher")
 
 -- create a hyper key for hotkeys
 HYPER = { "cmd", "alt", "ctrl", "shift" }
 
--- Terminal launcher (space and 2)
-local launchTerminal = function()
-	hs.execute("~/.config/hammerspoon/launch_show_hide.sh iTerm")
-end
-hs.hotkey.bind(HYPER, "space", launchTerminal)
-hs.hotkey.bind(HYPER, "2", launchTerminal)
+-- WORKSPACES --
+local AERO = "/opt/homebrew/bin/aerospace" -- adjust to your which-output
 
--- Application launchers
+local function aero(cmd)
+	hs.execute(AERO .. " " .. cmd)
+end
+
 hs.hotkey.bind(HYPER, "1", function()
-	hs.application.open("Brave Browser")
+	aero("workspace 1")
 end)
-
+hs.hotkey.bind(HYPER, "2", function()
+	aero("workspace 2")
+end)
 hs.hotkey.bind(HYPER, "3", function()
-	hs.application.open("Strongbox")
+	aero("workspace 3")
 end)
-
--- Todoist launcher (5 and t)
-local launchTodoist = function()
-	hs.execute("~/.config/hammerspoon/launch_show_hide.sh Todoist")
-end
-hs.hotkey.bind(HYPER, "5", launchTodoist)
-hs.hotkey.bind(HYPER, "t", launchTodoist)
 
 hs.hotkey.bind(HYPER, "4", function()
-	hs.execute("~/.config/hammerspoon/launch_show_hide.sh Obsidian")
+	aero("workspace 4")
+end)
+
+-- APPLICATION LAUNCHER --
+hs.hotkey.bind(HYPER, "space", function()
+	appLauncher.toggleApp("iTerm2")
+end)
+
+hs.hotkey.bind(HYPER, "t", function()
+	appLauncher.toggleApp("Todoist")
+end)
+
+hs.hotkey.bind(HYPER, "b", function()
+	appLauncher.toggleApp("Brave Browser")
+end)
+
+hs.hotkey.bind(HYPER, "f", function()
+	appLauncher.toggleApp("Finder")
+end)
+
+hs.hotkey.bind(HYPER, "o", function()
+	appLauncher.toggleApp("Obsidian")
 end)
 
 hs.hotkey.bind(HYPER, "s", function()
-	screenResolution.toggleScreenResolution()
+	appLauncher.toggleApp("Strongbox")
 end)
+
+hs.hotkey.bind(HYPER, "w", function()
+	appLauncher.toggleApp("WhatsApp")
+end)
+
+hs.hotkey.bind(HYPER, "p", function()
+	appLauncher.toggleApp("Spotify")
+end)
+
+hs.hotkey.bind(HYPER, "m", function()
+	appLauncher.toggleApp("Mail")
+end)
+
+hs.hotkey.bind(HYPER, "c", function()
+	appLauncher.toggleApp("Calendar")
+end)
+
+local HYPER = { "cmd", "alt", "ctrl", "shift" }
+local AERO = "/opt/homebrew/bin/aerospace" -- adjust if needed
+
+hs.hotkey.bind(HYPER, "r", function()
+	-- Reload both AeroSpace and Hammerspoon configs
+	hs.execute(AERO .. " reload-config")
+	hs.alert.show("Reloading configs...")
+	hs.reload()
+end)
+hs.alert.show("Config loaded")
 
 -- bind f6 to applescript
 hs.hotkey.bind("", "F6", function()
